58944087e2d6bacc10b1299678f55fc7dd3cbe0f
willian emboava <w.emboava.o@gmail.com>
2020-07-18 14:01:02 -0300

    added styles config

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import GlobalStyles from "./styles/global";
 
 function App() {
-  return <div>Oi</div>;
+  return (
+    <div>
+      <GlobalStyles />
+    </div>
+  );
 }
 
 export default App;
